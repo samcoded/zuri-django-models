522519114b5ca83faf704133429c63cf6a940e38
Samuel Anozie <samuelanozie10@yahoo.com>
2022-06-18 23:05:23 +0100

    Renamed model

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class BlogApp(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    created_date = models.DateTimeField('created date')
-    published_date = models.DateTimeField('published date')
-    body = models.TextField()
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Created_date = models.DateTimeField('created date')
+    Published_date = models.DateTimeField('published date')
     
     def __str__(self):
-        return self.title
+        return self.Title
