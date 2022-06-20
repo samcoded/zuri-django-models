2f21fcd88514d4995cc3ab64aff0f9b521de925d
Samuel Anozie <samuelanozie10@yahoo.com>
2022-06-20 15:43:56 +0100

    get_user_model to author field

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class PostDB(admin.ModelAdmin):
+    list_display = ('Title', 'Author', 'Created_date', 'Published_date')
+    
+admin.site.register(Post,PostDB)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     Created_date = models.DateTimeField('created date')
     Published_date = models.DateTimeField('published date')
     
